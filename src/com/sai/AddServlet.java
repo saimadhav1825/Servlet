bea4455b4a6da5833cf06f8219a6c5bbd589f9b5
saimadhav1825 <63303845+saimadhav1825@users.noreply.github.com>
2020-05-22 18:24:55 +0530

    Update AddServlet.java

--- src/com/sai/AddServlet.java
+++ src/com/sai/AddServlet.java
@@ -15,6 +15,30 @@ public class AddServlet  extends HttpServlet{
 		int k=i+j;
 		PrintWriter out=res.getWriter();
 		out.print(k);
+		
+		//two types we can call servlet to another servlet
+		//--->they are requestdispatcher,send redirect
+		
+  //1-->        -->requestdispatcher RequestDispatcher rd=req.getRequestDispatcher("square"); 
+	              //rd.forward(req, res);
+		          //req.setAttribute("k",k);
+		
+	//2-->       sendredirect are three types
+		            res.sendRedirect("square?k="+k);
+		        //session
+		       //cookies
+		      //session management
+		
+		         // 2.1--> HttpSession session=req.getSession();
+		         //session.setAttribute("k", k);
+		          //2.2--> Cookie cookie=new Cookie("k", k+" ");
+		           //res.addCookie(cookie);
+		           
+		          // 2.3-->session maanagement 
+		          // res.sendRedirect("square?k="+k);
+		//PrintWriter out=res.getWriter();
+		//out.print(k);
+	}
 	}
 
 }
